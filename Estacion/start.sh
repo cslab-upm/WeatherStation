c39af5d1c289e1e0be21d55225d52037b7ab13a0
Miguel García <gtaboadamiguel@gmail.com>
2017-12-14 16:36:24 +0100

    Script de inicio
    El script de inicio ahora espera a que terminen los procesos a los que llama

--- Estacion/start.sh
+++ Estacion/start.sh
@@ -6,9 +6,20 @@ touch STATUS.txt
 
 # Iniciar el cliente serie para comunicarse de forma inalámbrica con la estación meteorológica
 echo " [Estacion] Iniciando comunicación con la estación meteorológica"
-python Serial/WeatherSerial.py > /dev/null &
+python Serial/WeatherSerial.py  & pid_serial=$!
 
+echo "PID del monitor serial: $pid_serial"
 
 # Iniciar el servidor para la API
 echo "[Estacion] Iniciando API REST"
-python API/WeatherAPIv2.py 
+python API/WeatherAPIv2.py & pid_api=$!
+
+echo "PID del servidor de la api: $pid_api"
+
+wait $pid_serial
+
+ret_serial=$?
+
+if [ $ret_serial -ne 0 ];then
+    kill $pid_api
+fi
